08e621010381a35dabf583ce726dac420b6f18d2
Salo <salomonco@gmail.com>
2021-02-18 21:46:01 +0100

    added hash function

--- HashTable.py
+++ HashTable.py
@@ -25,12 +25,22 @@ class HashTable:
 
 
 
-  # 2️⃣ TODO: Create your own hash function.
+  # 2️⃣ : Create your own hash function.
 
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
   def hash_func(self, key):
-    pass
+     # 1. Get the first letter of the key and lower case it
+    first_letter = key[0].lower()
+
+    # 2. Calculate the distance from letter a
+    distance_from_a = ord(first_letter) - ord('a')
+
+    # 3. Mod it to make sure it is in range
+    index = distance_from_a % self.size
+
+    # returns index
+    return index
 
 
 
